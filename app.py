08c461c3e4e766c514f3567153ddec5b16e70a74
Dhanush <dhanushbvb@gmail.com>
2026-05-19 19:25:38 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 from utils.db import users_collection, predictions_collection
@@ -328,4 +330,5 @@ def predict_next_month():
 # =====================================================
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
